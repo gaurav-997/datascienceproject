4bfefeb190192164f48ce746d360e1d7e87e1853
gaugaura <gaurav.2.gaurav@nokia.com>
2025-08-10 20:29:54 +0530

    adding Project structure

---
+++ README.md
@@ -0,0 +1,7 @@
+echo "# datascienceproject" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/gaurav-997/datascienceproject.git
+git push -u origin main
---
+++ template.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+import logging
+
+project_name = "datascience"
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",  # here we define the constructor bcz with this we can import this project anywhere 
+    f"src/{project_name}/components/__init__.py",   # this will be used for different stages of pipeline
+    f"src/{project_name}/utils/__init__.py",  # this is used for any common functiontanility
+    f"src/{project_name}/components/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/components/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/components/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "DockerFile",
+    "requirments.txt",
+    "setup.py",
+    "resarch/resarch.ipynb",
+    "templates/index.html",
+    "app.py"
+]
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s')
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    filedir,filename = os.path.split(file_path)
+    
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"creating directory {filedir} for file:{filename}")
+    
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0) :
+        with open(file_path,"w") as f:
+            pass
+            logging.info(f"creating empty file:{file_path}")
+    else:
+        logging.info(f"file : {file_path} already exists")
